cba71f628cb20ad3a812ce53c0624e55f05038b2
João Avelino <joaolavelino@gmail.com>
2026-09-03 16:01:16 -0300

    full scale schedule

--- src/components/schedule/GigLine.tsx
+++ src/components/schedule/GigLine.tsx
@@ -15,11 +15,11 @@ export function GigLine({ gig, index }: GigLineProps) {
     return (
         <div className={`flex flex-col items-start ${isEven ? 'text-brand border-brand' : 'text-white border-white'} border-b-8`}>
             <div className="w-full flex justify-between items-center ">
-                <p className="font-display text-2xl text-center leading-4">{format(gig.date, 'dd/MM - HH:mm')}</p>
-                <p className="font-display text-2xl  text-center leading-0">{FORMATS[gig.format]}</p>
+                <p className="font-display text-2xl text-center ">{format(gig.date, 'dd/MM - HH:mm')}</p>
+                <p className="font-display text-2xl  text-center ">{FORMATS[gig.format]}</p>
             </div>
 
-            <p className="font-display text-2xl text-cente leading-tight">
+            <p className="font-display text-2xl text-cente ">
                 {gig.venue}
                 <span className="ml-4 text-lg">
                     (
